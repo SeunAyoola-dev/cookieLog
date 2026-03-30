bd9adb495f34aefab49e839463b220289d86464e
SeunAyoola-dev <seunayoola22@icloud.com>
2026-03-30 12:59:17 +0100

    fixing a small error

--- processor.py
+++ processor.py
@@ -3,7 +3,7 @@ from helpers.helpers import parse_date
 logger = logging.getLogger(__name__)
 
 def parse_input_file(file_path, target_date):
-    logger.debug(f"[INFO] Parsing input file: {file_path}")
+    logger.debug(f"Parsing input file: {file_path}")
     counts = {}
 
     with open(file_path, "r") as file:
@@ -21,11 +21,11 @@ def parse_input_file(file_path, target_date):
 
 
                 elif date < target_date:
-                    logger.debug(f"[INFO] Early exit at line {line_num} because date {date} < target_date {target_date}")
+                    logger.debug(f"Early exit at line {line_num} because date {date} < target_date {target_date}")
                     break # break early
 
             except Exception as e:
-                logger.warning(f"[WARNING] Skipping malformed line {line_num}: {line.strip()} - {e}")
+                logger.warning(f"Skipping malformed line {line_num}: {line.strip()} - {e}")
                 continue
 
 
@@ -33,22 +33,22 @@ def parse_input_file(file_path, target_date):
 
 def find_most_active_cookies(counts):
     if not counts:
-        logger.debug("[WARNING] find_most_active_cookies called with empty counts")
+        logger.debug("find_most_active_cookies called with empty counts")
         return []
 
     most_active_count = max(counts.values())
     return [cookie for cookie, count in counts.items() if count == most_active_count]
 
 def process(file, date):
-    logger.debug(f"[INFO] Starting processing for date: {date}")
+    logger.debug(f"Starting processing for date: {date}")
     counts = parse_input_file(file, date)
     
     if not counts:
-        logger.warning(f"[WARNING] No cookies found for date: {date}")
+        logger.warning(f"No cookies found for date: {date}")
 
 
     most_active_cookies = find_most_active_cookies(counts)
-    logger.info(f"[INFO] Found {len(most_active_cookies)} most active cookie(s) for {date}")
+    logger.info(f"Found {len(most_active_cookies)} most active cookie(s) for {date}")
 
     for cookie in most_active_cookies:
         print(cookie)
